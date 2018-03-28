e7813ca3b09849682b1ac2cfb78af0591e574578
pixelbrackets <mail@pixelbrackets.de>
2018-03-28 15:18:55 +0200

    [FEATURE] Add poll dummy
    Add a basic poll controller, which handles root requests
    by offering available endpoints and requests to generate a poll
    by answering with a dummy message.

---
+++ src/Controller/PollController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Flex\Response;
+
+class PollController extends Controller
+{
+    /**
+     * @Route("/", name="root")
+     */
+    public function index()
+    {
+        return $this->json([
+            'meta' => [
+                'description' => 'Available endpoints in this app',
+                'resources' => [
+                    'poll' => $this->generateUrl('poll-new')
+                ]
+            ],
+            'links' => [
+                'self' => $this->generateUrl('root')
+            ]
+        ]);
+    }
+
+    /**
+     * Create poll with incoming slashcommand webhook
+     *
+     * @Route("/new", name="poll-new", methods="POST")
+     */
+    public function new(Request $request)
+    {
+        $requestContent = $request->getContent() ?: [];
+        $json = json_decode($requestContent, true);
+        if (false === isset($json['channel_id'], $json['user_id'], $json['command'])) {
+            return $this->json([
+                'errors' => [
+                    'status' => \Symfony\Component\HttpFoundation\Response::HTTP_BAD_REQUEST,
+                    'detail' => 'Missing required parameters'
+                ]],
+                \Symfony\Component\HttpFoundation\Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $text = 'Hello World';
+        if ($json['channel_id'] === 'niah6qa') {
+            $text = 'Hi Team! 😀';
+        }
+
+        return $this->json([
+            'response_type' => 'in_channel',
+            'text' => $text
+        ]);
+    }
+}
